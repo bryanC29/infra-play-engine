a7db91a8a2fa215906e97f9943585ddd3c08d7e8
Bryan Christy <bryanchristy2016@gmail.com>
2025-08-26 21:48:55 +0530

    ADD: DTO Validator

--- simulation-engine/api/handleSimulation.go
+++ simulation-engine/api/handleSimulation.go
@@ -5,93 +5,26 @@ import (
 	"net/http"
 	"simengine/simulator"
 	"simengine/types"
+
+	"github.com/go-playground/validator/v10"
 )
 
+var validate = validator.New()
+
 func handleSimulation(w http.ResponseWriter, r *http.Request) {
+	var problem types.Problem
 
-	data := []byte(`{
-		"nodes": [
-			{
-				"id": "entry",
-				"type": "Entry",
-				"resources": {
-					"cpu": 1,
-					"memoryMB": 1,
-					"replicas": 1
-				}
-			},
-			{
-				"id": "lb-1",
-				"type": "LoadBalancer",
-				"resources": {
-					"cpu": 1,
-					"memoryMB": 256,
-					"replicas": 1
-				}
-			},
-			{
-				"id": "api-1",
-				"type": "API",
-				"resources": {
-					"cpu": 1.0,
-					"memoryMB": 1024,
-					"replicas": 2
-				}
-			},
-			{
-				"id": "queue-1",
-				"type": "Queue",
-				"resources": {
-					"cpu": 0.5,
-					"memoryMB": 512,
-					"replicas": 1
-				}
-			},
-			{
-				"id": "worker-1",
-				"type": "Worker",
-				"resources": {
-					"cpu": 1.5,
-					"memoryMB": 2048,
-					"replicas": 3
-				}
-			},
-			{
-				"id": "db-1",
-				"type": "Database",
-				"resources": {
-					"cpu": 2.0,
-					"memoryMB": 4096,
-					"replicas": 1
-				}
-			},
-			{
-				"id": "exit",
-				"type": "Exit",
-				"resources": {
-					"cpu": 1,
-					"memoryMB": 1,
-					"replicas": 1
-				}
-			}
-		],
-		"connections": [
-			{ "from": "entry", "to": "lb-1" },
-			{ "from": "lb-1", "to": "api-1" },
-			{ "from": "api-1", "to": "queue-1" },
-			{ "from": "queue-1", "to": "worker-1" },
-			{ "from": "worker-1", "to": "db-1" },
-			{ "from": "db-1", "to": "exit" }
-		]
-	}`)
-
-	var design types.Design
-	if err := json.Unmarshal(data, &design); err != nil {
-		panic(err)
+	if err := json.NewDecoder(r.Body).Decode(&problem); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	
+	if err := validate.Struct(problem); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
 	}
 
-	simulator.EngineRun(design)
+	simulator.EngineRun(problem.Design)
 
 	RespondWithJSON(w, 200, map[string]string{"msg":"done"})
-
 }
--- simulation-engine/types/design.go
+++ simulation-engine/types/design.go
@@ -1,26 +1,26 @@
 package types
 
 type Design struct {
-	Nodes       []Node       `json:"nodes"`       // All components in the architecture
-	Connections []Connection `json:"connections"` // Directed edges between nodes
+	Nodes       []Node       `json:"nodes" validate:"required,dive"`       // All components in the architecture
+	Connections []Connection `json:"connections" validate:"required,dive"` // Directed edges between nodes
 }
 
 // Node defines a single infrastructure component in the design.
 type Node struct {
-	ID        string        `json:"id"`   // Unique identifier for the node
-	Type      string        `json:"type"` // E.g., "Database", "App", "Cache"
-	Resources NodeResources `json:"resources"`
+	ID        string        `json:"id" validate:"required"`   // Unique identifier for the node
+	Name      string        `json:"name" validate:"required"` // E.g., "Database", "App", "Cache"
+	Resources NodeResources `json:"resources" validate:"required"`
 }
 
 // NodeResources represents the resource configuration for a component.
 type NodeResources struct {
-	CPU      float64 `json:"cpu"`      // Per replica (in vCPUs)
-	MemoryMB int     `json:"memoryMB"` // Per replica (in MB)
-	Replicas int     `json:"replicas"` // Number of instances of the node
+	CPU      float64 `json:"cpu" validate:"required"`      // Per replica (in vCPUs)
+	MemoryMB int     `json:"memoryMB" validate:"required"` // Per replica (in MB)
+	Replicas int     `json:"replicas" validate:"required"` // Number of instances of the node
 }
 
 // Connection represents a directed edge from one node to another.
 type Connection struct {
-	From string `json:"from"` // Source node ID
-	To   string `json:"to"`   // Target node ID
+	From string `json:"from" validate:"required"` // Source node ID
+	To   string `json:"to" validate:"required"`   // Target node ID
 }
