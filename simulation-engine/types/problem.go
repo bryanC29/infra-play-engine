bc68779799d4055b2a9831178208023b3698174a
Bryan Christy <bryanchristy2016@gmail.com>
2025-09-01 15:39:27 +0530

    FIX: Problem shape

--- simulation-engine/types/problem.go
+++ simulation-engine/types/problem.go
@@ -1,10 +1,7 @@
 package types
 
 type Problem struct {
-	UserID               string  `json:"userId" validate:"required"`          // ID of the user submitting the design
-	SubmissionID         string  `json:"submissionId" validate:"required"`    // Magic number to track a unique submission
-	BaseQPS              int     `json:"baseQps" validate:"required"`         // Baseline QPS to simulate
-	RequiredAvailability float64 `json:"reqAvailability" validate:"required"` // Target availability (e.g., 0.99)
-	RequiredLatencyMS    int     `json:"reqLatencyMs" validate:"required"`    // Max allowed average latency in milliseconds
-	Design               Design  `json:"design" validate:"required"`
+	SubmissionID string `json:"submissionId" validate:"required"` // Magic number to track a unique submission
+	BaseQPS      int    `json:"baseQps" validate:"required"`      // Baseline QPS to simulate
+	Design       Design `json:"design" validate:"required"`
 }
