845bb158a150c5ff007aa8748f68036ad496a5e4
Bryan Christy <bryanchristy2016@gmail.com>
2025-09-04 00:24:08 +0530

    FIX: Project files

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,16 @@
+/* eslint-disable prettier/prettier */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const configService = app.get(ConfigService);
+  const PORT = configService.get<number>('BACKEND_PORT') ?? 3000;
+
+  await app.listen(PORT);
+
+  console.log(`Server running on port: ${PORT}`);
 }
-bootstrap();
+
+void bootstrap();
