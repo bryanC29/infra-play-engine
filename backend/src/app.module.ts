df5cd7ffc5db2c82162a85ff7f362151d02123fe
Bryan Christy <bryanchristy2016@gmail.com>
2025-09-13 21:26:49 +0530

    ADD: Mongoose and Pipes

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,13 +4,17 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { SimulateModule } from './simulate/simulate.module';
+import { AuthModule } from './auth/auth.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    MongooseModule.forRoot('mongodb://localhost:27017/infra'),
     SimulateModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,11 +2,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   const PORT = configService.get<number>('BACKEND_PORT') ?? 3000;
+
+  app.enableCors();
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
 
   await app.listen(PORT);
 
